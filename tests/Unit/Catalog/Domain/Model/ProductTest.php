1e85a36104d33103d0dac5b36b21eb4020f45588
Bartlomiej Hacas <bhacas@gmail.com>
2025-08-05 18:36:03 +0200

    added xdebug

---
+++ tests/Unit/Catalog/Domain/Model/ProductTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests\Unit\Catalog\Domain\Model;
+
+use App\Catalog\Domain\Model\Product;
+use App\Shared\Domain\Model\Price;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Uid\Uuid;
+
+class ProductTest extends TestCase
+{
+    public function testProductIsInstantiatedCorrectlyWithConstructor(): void
+    {
+        $productName = 'Nowoczesna Klawiatura';
+        $price = new Price(34999, 'PLN'); // 349.99 PLN
+        $stock = 50;
+
+        $product = new Product($productName, $price, $stock);
+
+
+        $this->assertInstanceOf(Uuid::class, $product->getId());
+
+        $this->assertSame($productName, $product->getName());
+
+        $this->assertSame($price, $product->getPrice());
+        $this->assertSame(34999, $product->getPrice()->getAmount());
+        $this->assertSame('PLN', $product->getPrice()->getCurrency());
+
+        $this->assertSame($stock, $product->getStock());
+
+        $this->assertNull($product->getDescription());
+    }
+}
